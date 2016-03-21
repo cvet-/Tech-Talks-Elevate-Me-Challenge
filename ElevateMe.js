497f05e690b5a1ad19a0f51f2a66fb3a9dc4c4b6
Cvetelina Mladenova <cvetelina_ml@yahoo.com>
2016-03-21 13:41:41 +0200

    commit ElevateMe.js

---
+++ ElevateMe.js
@@ -0,0 +1,21 @@
+﻿var elevatorCode = {
+    init: function (elevators, floors) {
+        var rotator = 0;                   // Rotator variable to distribute the workload. 
+        _.each(floors, function (floor) {  // Using lo-dash to iterate over all the elevators and floors
+            floor.on("up_button_pressed down_button_pressed", function () {
+                var elevator = elevators[(rotator++) % elevators.length];
+                elevator.goToFloor({ number: floor.level });
+            });
+        });
+        _.each(elevators, function (elevator) {
+            elevator.on("floor_button_pressed", function (floorNum) {
+                elevator.goToFloor({ number: floorNum });
+            });
+            elevator.on("idle", function () {
+                elevator.goToFloor({ number: 0 });
+            });
+        });
+    },
+    update: function (dt, elevators, floors) {
+    }
+}
